c7c385aa7ee42395a68635607c07b21700e8d973
Anderson Freires <55098476+dre36dre@users.noreply.github.com>
2024-07-16 21:20:00 -0300

    Adicionando métodos tocar e pausar

--- ReprodutorMusical.java
+++ ReprodutorMusical.java
@@ -1,7 +1,31 @@
 public class ReprodutorMusical{
     public static void main(String[] args){
+        private String musica;
+        private boolean tocando=false;
+        
         public  tocar(){
+           if(tocando == true){
             System.out.println("Tocando musica");
+           }else{
+               System.out.println("Musica esta em pausa");
+           }
         }
-    }
+        //Método pausar
+        public pausar(){
+            if(tocando != true ){
+                System.out.println("Musica em pausa");
+            }else{
+                System.out.println("Musica esta em pausa");
+            }
+        }
+        //Método selecionar musica
+            public selecionarMusica(String musica){
+                this.musica=musica;
+            }
+            public void setMusica(String musica){
+                this.musica=musica;
+        }
+            public String getMusica(){
+                return this.musica;
+            }
 }
